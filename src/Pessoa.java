59fbbe0b380569472d6b47bd9de8b1dc5f4adced
Allan Freire(ACBZ) <Allan_Freire@acbz.com.br>
2024-02-06 09:33:42 -0300

    exemplos de array

---
+++ src/Pessoa.java
@@ -0,0 +1,20 @@
+public class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Pessoa (String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + nome + " (Idade: " + idade + ")";
+    }
+
+}
--- src/TesteGeral.java
+++ src/TesteGeral.java
@@ -1,28 +1,36 @@
 import br.com.alura.screenmatch.modelos.Filme;
 
+import java.util.ArrayList;
+
 public class TesteGeral {
     public static void main(String[] args) {
 
+        Pessoa pessoa1 = new Pessoa("Karina Juliana", 44);
+        Pessoa pessoa2 = new Pessoa("Brenda Freire", 7);
+        Pessoa pessoa3 = new Pessoa("Allan Freire",43);
 
+        ArrayList<Pessoa> listaPessoa = new ArrayList<>();
 
+        listaPessoa.add(pessoa1);
+        listaPessoa.add(pessoa2);
+        listaPessoa.add(pessoa3);
+
+        System.out.println("Tamanho da lista " + listaPessoa.size());
+        System.out.println("Primeiro nome " + listaPessoa.get(0).getNome());
+        System.out.println("Primeiro pessoa " + listaPessoa.get(0));
+        System.out.println(listaPessoa);
+        System.out.println("toString do filme " + listaPessoa.get(0).toString());
 
         //Produto produto = new Produto();
-
         //Carrinho carrinho = new Carrinho();
-
         //carrinho.adicionaProduto(produto);
 
-
-
         /*int[] numeros = new int[5];
-
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = i + 1;
         }
         System.out.println(numeros[0]);
 */
-
-
         /*
         criando array instanciando parametros
         Filme[] filmes = new Filme[2];
@@ -38,6 +46,5 @@ public class TesteGeral {
         System.out.println(filme1.getNome());
         System.out.println(filme1.getAnoDeLancamento());
 */
-
     }
 }
